e88cc11ae6da8d6a8542eddc3dfe83f93859ac3c
KarenAmicone <karen_amicone@hotmail.com>
2020-05-27 12:42:16 -0500

    The Update quantity action was added to the reducer and implemented in the Ticket component

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -8,7 +8,17 @@ class Ticket extends React.Component {
     this.props.deleteOrder(id, price);
   };
 
-  handleOrder = e => {
+  handleAddItem = (id, quant) => {
+    let newQuant = quant + 1;
+    this.props.updateQuant(id, newQuant);
+  };
+
+  handleSubstractItem = (id, quant) => {
+    let newQuant = quant - 1;
+    this.props.updateQuant(id, newQuant);
+  };
+
+  handleOrder = (e) => {
     e.preventDefault();
     app
       .firestore()
@@ -16,7 +26,8 @@ class Ticket extends React.Component {
       .add({
         orders: this.props.orders,
         total: this.props.total,
-        clientName: this.props.clientName
+        clientName: this.props.clientName,
+        createdAt: app.firestore.Timestamp.fromDate(new Date()),
       })
       .then(this.props.reset());
   };
@@ -25,34 +36,47 @@ class Ticket extends React.Component {
     return (
       <section id="payments">
         <article id="table">
-          <table>
-            <tbody>
-              {this.props.orders.map(order => (
-                <tr key={order.id}>
-                  <td>{order.name}</td>
-                  <td>{"$" + order.price}</td>
-                  <td>{order.ingredients}</td>
-                  <td>{order.quant}</td>
-                  <td>
-                    {" "}
-                    <button
-                      onClick={() => {
-                        this.handleDelete(order.id, order.price);
-                      }}
-                    >
-                      <i className="material-icons">delete_outline</i>
-                    </button>{" "}
-                  </td>
-                </tr>
-              ))}
-
-              <tr key="total">
-                <td>Total:</td>
-                <td>{"$" + this.props.total}</td>
-              </tr>
-              <tr></tr>
-            </tbody>
-          </table>
+          {this.props.orders.map((order) => (
+            <div key={order.id} className="order-item-container">
+              <p className="order-item-name ticket-element">{order.name}</p>
+              <p className="order-item-price ticket-element">
+                {`$ ${order.price}`}
+              </p>
+              <p className="order-item-quantity ticket-element">
+                {order.quant}
+              </p>
+              <div className="counter-buttons-container">
+                <button
+                  onClick={() => {
+                    this.handleSubstractItem(order.id, order.quant);
+                  }}
+                >
+                  -
+                </button>
+                <button
+                  onClick={() => {
+                    this.handleAddItem(order.id, order.quant);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+              <div className="order-item-delete-bttn ticket-element">
+                <button
+                  onClick={() => {
+                    this.handleDelete(order.id, order.price);
+                  }}
+                >
+                  <i className="material-icons">delete_outline</i>
+                </button>
+              </div>
+              <p className="order-item-specifications ticket-element">{`DETALLES: ${order.ingredients}`}</p>
+            </div>
+          ))}
+          <div key="total" className="total">
+            <p>Total:</p>
+            <p>{`$ ${this.props.total}`}</p>
+          </div>
         </article>
         <article className="ordering">
           <button className="purple-bttns" onClick={this.handleOrder}>
@@ -64,15 +88,15 @@ class Ticket extends React.Component {
   }
 }
 
-const mapState = state => {
+const mapState = (state) => {
   return {
     orders: state.orders,
     total: state.total,
-    clientName: state.clientName
+    clientName: state.clientName,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     deleteOrder: (id, price) => {
       dispatch({ type: "DELETE_ORDER", id: id, price: price });
@@ -80,7 +104,10 @@ const mapDispatchToProps = dispatch => {
     reset: () => {
       dispatch({ type: "RESET" });
     },
-    addItems: () => {}
+    addItems: () => {},
+    updateQuant: (id, quant) => {
+      dispatch({ type: "UPDATE_QUANTITY", id: id, quant: quant });
+    },
   };
 };
 
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,69 +1,76 @@
-import menu from '../components/menu.json'
+import menu from "../components/menu.json";
 
 const initState = {
-    bttns: menu.bttns,
-    options: menu.options,
-    lunchOptions: menu.lunchOptions,
-    orders: [],
-    total: 0,
-    path: null,
-    clientName: "",
+  bttns: menu.bttns,
+  options: menu.options,
+  lunchOptions: menu.lunchOptions,
+  orders: [],
+  total: 0,
+  path: null,
+  clientName: "",
 };
 
 const rootReducer = (state = initState, action) => {
-    if(action.type === 'ADD_ORDER'){
-        let newOrder = [...state.orders, action.order]
-            return {
-                ...state,
-                orders: newOrder
-            }
-    } else if(action.type === 'DELETE_ORDER'){
-        let newOrder = state.orders.filter(order=>{
-            return action.id !== order.id
-          });
+  if (action.type === "ADD_ORDER") {
+    let newOrder = [...state.orders, action.order];
+    return {
+      ...state,
+      orders: newOrder,
+    };
+  } else if (action.type === "DELETE_ORDER") {
+    let newOrder = state.orders.filter((order) => {
+      return action.id !== order.id;
+    });
 
-        let sum = state.total - action.price
-          return {
-              ...state,
-            orders: newOrder,
-            total: sum
-          }
-    } else if (action.type === 'COUNT'){
-        let prices = state.orders.map(order => {
-            return parseInt(order.price) 
-        })
-        let sum = prices.reduce((total, num)=> {
-            return total + num
-        });
-        
-        return {
-            ...state,
-            total: sum
-        }
-    } else if(action.type === 'EDIT_INGREDIENT'){
-        let newOrder = [...state.orders];
-        return {
-            ...state,
-          orders: newOrder
-        }
-    } else if(action.type === 'ADD_NAME'){
-        let name = action.name
-        return{
-            ...state,
-            clientName: name
-        }
-    } else if(action.type === 'RESET'){
-        return {
-            bttns: menu.bttns,
-            options: menu.options,
-            lunchOptions: menu.lunchOptions,
-            orders: [],
-            total: 0,
-            path: null,
-            clientName: ""
-        }
-    }
-    return state;
+    let sum = state.total - action.price;
+    return {
+      ...state,
+      orders: newOrder,
+      total: sum,
+    };
+  } else if (action.type === "COUNT") {
+    let prices = state.orders.map((order) => {
+      return parseInt(order.price);
+    });
+    let sum = prices.reduce((total, num) => {
+      return total + num;
+    });
+
+    return {
+      ...state,
+      total: sum,
+    };
+  } else if (action.type === "EDIT_INGREDIENT") {
+    let newOrder = [...state.orders];
+    return {
+      ...state,
+      orders: newOrder,
+    };
+  } else if (action.type === "ADD_NAME") {
+    let name = action.name;
+    return {
+      ...state,
+      clientName: name,
+    };
+  } else if (action.type === "UPDATE_QUANTITY") {
+    return {
+      ...state,
+      orders: state.orders.map((order) =>
+        order.id === action.id ? { ...order, quant: action.quant } : order
+      ),
+    };
+  } else if (action.type === "RESET") {
+    return {
+      bttns: menu.bttns,
+      options: menu.options,
+      lunchOptions: menu.lunchOptions,
+      orders: [],
+      total: 0,
+      path: null,
+      clientName: "",
+    };
+  }
+  return state;
 };
 
-export default rootReducer
+export default rootReducer;
